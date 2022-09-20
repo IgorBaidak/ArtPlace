02c2d8309c4f0bee2cb3d707164f2e336d245fd7
IgorBaidak <igorbaidak@gmail.com>
2022-09-20 23:59:45 +0300

    addFirebase

--- ArtPlace/ArtPlace/Delegates/AppDelegate.swift
+++ ArtPlace/ArtPlace/Delegates/AppDelegate.swift
@@ -6,17 +6,30 @@
 //
 
 import UIKit
+import FirebaseCore
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+//@main
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//
+//
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//
+//        return true
+//    }
 
+    @UIApplicationMain
+    class AppDelegate: UIResponder, UIApplicationDelegate {
 
+      var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+                       [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
+      }
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -31,6 +44,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
-}
 
